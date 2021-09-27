6e8fb5f24171b7a0c467fac192c795b4de42b2fe
BeTaJI-JS <PeaceDeathinmylife@gmail.com>
2021-09-27 13:21:02 +0300

    add getWeather

---
+++ bot.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const {getWeather} = require("./lib/weather-api")
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/weather (.+)/, async (msg, match) =>{
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId,  await  getWeather(match [1]))
+})
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
